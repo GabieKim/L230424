e4578b65f48ab79d70396b28af0f7d543160cc8c
GabieKim <rumisonic@gmail.com>
2023-04-24 21:52:02 +0900

    2. CPP 파일 생성함(폰에 비행기 컴포넌트 추가해둠)

--- Source/L230424/MyGameModeBase.cpp
+++ Source/L230424/MyGameModeBase.cpp
@@ -2,4 +2,16 @@
 
 
 #include "MyGameModeBase.h"
+#include "MyPlayerController.h"
+#include "MyPawn.h"
 
+
+// CDO(클래스 디폴트 오브젝트) 초기화 
+AMyGameModeBase::AMyGameModeBase()
+{
+	// 클래스 이름 기록
+	PlayerControllerClass = AMyPlayerController::StaticClass();
+	DefaultPawnClass = AMyPawn::StaticClass();
+
+
+}
--- Source/L230424/MyGameModeBase.h
+++ Source/L230424/MyGameModeBase.h
@@ -12,6 +12,10 @@
 UCLASS()
 class L230424_API AMyGameModeBase : public AGameModeBase
 {
-	GENERATED_BODY()
+	// 리플렉션 코드 만들 때 자동으로 여기에 붙는다
+	GENERATED_BODY()	
+public:
+	AMyGameModeBase();
 	
+
 };
